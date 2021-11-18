64ce4366af9538c5d7a180ff4a498f22a207a65c
Jeordane Batista <jeordane.batista@hotmail.com>
2021-11-18 16:01:58 -0300

    Add basic middlewares

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import setupStaticFiles from '@main/config/static-files';
+import setupMiddleware from '@main/config/middlewares';
 
 export const setupApp = async (): Promise<Express> => {
   const app: Express = express();
   setupStaticFiles(app);
+  setupMiddleware(app);
 
   return app;
 };
