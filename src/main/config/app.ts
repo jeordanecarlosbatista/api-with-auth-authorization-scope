d13bfed2ad5f6eb136496b5c7e31f8e981a6e844
Jeordane Batista <jeordane.batista@hotmail.com>
2021-11-18 15:58:22 -0300

    Add route static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express, { Express } from 'express';
+import setupStaticFiles from '@main/config/static-files';
 
 export const setupApp = async (): Promise<Express> => {
   const app: Express = express();
+  setupStaticFiles(app);
 
   return app;
 };
