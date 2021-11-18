83f2a5520176a61365c94f6099d8d4fa48abca84
Jeordane Batista <jeordane.batista@hotmail.com>
2021-11-18 16:10:06 -0300

    Add doc swagger and configuration route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 import setupStaticFiles from '@main/config/static-files';
 import setupMiddleware from '@main/config/middlewares';
+import setupSwagger from '@main/config/swagger';
 
 export const setupApp = async (): Promise<Express> => {
   const app: Express = express();
   setupStaticFiles(app);
+  setupSwagger(app);
   setupMiddleware(app);
 
   return app;
---
+++ src/main/docs/index.ts
@@ -0,0 +1,26 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'API Rest',
+    description: 'Essa é a documentação da API feita pelo Jeordane Batista',
+    version: '1.0.0',
+    contact: {
+      name: 'Jeordane Carlos Batista',
+      email: 'jeordane.batista@icloud.com.br',
+      url: 'https://www.linkedin.com/in/jeordane-batista-4a7b70117',
+    },
+    license: {
+      name: '',
+      url: '',
+    },
+  },
+  externalDocs: {
+    description: '',
+    url: '',
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal',
+  }],
+  tags: [],
+};
