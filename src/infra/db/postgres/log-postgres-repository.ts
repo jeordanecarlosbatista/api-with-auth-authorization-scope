0b301a74754859074dc4553fc3c5e078796b930f
Jeordane Batista <jeordane.batista@hotmail.com>
2021-12-01 14:48:47 -0300

    Refactoring

--- src/infra/db/postgres/log-postgres-repository.ts
+++ src/infra/db/postgres/log-postgres-repository.ts
@@ -3,6 +3,11 @@ import { PostgresHelper } from '@infra/db/postgres';
 
 export class LogPostgresRepository implements LogErrorRepository {
   async logError(stack: string): Promise<void> {
-    await PostgresHelper.client.log.create({ data: { stack } });
+    try {
+      await PostgresHelper.client.log.create({ data: { stack } });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
   }
 }
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 export default {
-  jwtSecret: 'abcde',
+  jwtSecret: process.env.JWT_SECRET || 'abcdef',
   port: process.env.PORT || 3333,
   postgresUrl: process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5432/helloworld',
 };
